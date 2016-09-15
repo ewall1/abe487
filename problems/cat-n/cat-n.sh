aaf0cacda81af3ce89796ea7c00ac73da07ea642
Emily Wall <ewall@service2.ice.americas.sgi.com>
2016-09-14 20:18:36 -0700

    added cat-n

--- problems/cat-n/cat-n.sh
+++ problems/cat-n/cat-n.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo "OK"
+set -u 
+
+if [[ $# -lt 1 ]]; then
+
+printf "Usage: %s requires that a file be submitted for cat-n\n" $0
+
+exit 1
+
+fi
+
+
+i=0
+
+while read line
+do
+
+let i++
+printf "%d %s\n" $i $line
+
+done<*.txt
